5276cd6927a4656305c8018bb1d86a81b891783f
Guido Toledo Gonzalez <toledoguido@gmail.com>
2026-06-11 10:14:33 -0300

    Create funciones.py
    Continuo con la estructura básica de carpetas y archivos inicializando el .py dedicado a funciones

---
+++ buscador_de_paises/modulos/funciones.py
@@ -0,0 +1,17 @@
+def cargar_csv():
+def guardar_csv():
+def mostrar_menu():
+
+def agregar_pais():
+def actualizar_pais():
+def buscar_pais()
+
+def filtrar_continente():
+def filtrar_poblacion()
+def filtrar_superficie()
+
+def ordenar_nombre()
+def ordenar_poblacion()
+def ordenar_superficie()
+
+def estadisticas()
